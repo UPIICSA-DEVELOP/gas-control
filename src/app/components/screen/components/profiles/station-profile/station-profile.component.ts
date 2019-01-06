f402775885fc03d86493358334438ff34132c8eb
Fercho10 <feruriel00@hotmail.com>
2019-01-06 17:29:45 -0600

    feat: add station and user profile component

---
+++ src/app/components/screen/components/profiles/station-profile/station-profile.component.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) MapLander S de R.L de C.V - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-station-profile',
+  templateUrl: './station-profile.component.html',
+  styleUrls: ['./station-profile.component.scss']
+})
+export class StationProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/screen/components/profiles/user-profile/user-profile.component.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) MapLander S de R.L de C.V - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
