11b26ba2445f764cc476a349cec083a3fbc2b2a9
Fercho10 <feruriel00@hotmail.com>
2019-01-11 09:23:45 -0600

    fix: change notification turn on-off

--- src/app/components/screen/components/station-list/station-list.component.ts
+++ src/app/components/screen/components/station-list/station-list.component.ts
@@ -4,34 +4,38 @@
  * Proprietary and confidential
  */
 
-import {AfterViewInit, Component, DoCheck, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ApiService} from '@app/core/services/api/api.service';
 import {DialogService} from '@app/core/components/dialog/dialog.service';
 import {CookieService} from '@app/core/services/cookie/cookie.service';
 import {Constants} from '@app/core/constants.core';
-import {SessionStorageService} from '@app/core/services/session-storage/session-storage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-station-list',
   templateUrl: './station-list.component.html',
   styleUrls: ['./station-list.component.scss']
 })
-export class StationListComponent implements OnInit, DoCheck {
-  public user: any;
-  public consultancyBasicData: any;
+export class StationListComponent implements OnInit {
+
+  @Input() public stationList: any[];
+  public notificationActive: boolean[] = [];
   public groupIcon: any;
 
   constructor(
     private _api: ApiService,
-    private _dialogService: DialogService
+    private _dialogService: DialogService,
+    private _router: Router
   ) {
   }
 
   ngOnInit() {
-    this.getStations();
-  }
-
-  ngDoCheck(): void {
+    if (this.stationList) {
+      for (let i = 0; i < this.stationList.length; i++){
+        this.notificationActive.push(this.stationList[i].activeNotification);
+      }
+    }
+    this.getUtilities();
   }
 
   private getUtilities():void{
@@ -40,34 +44,8 @@ export class StationListComponent implements OnInit, DoCheck {
     })
   }
 
-  private getStations(): void {
-    this.getUtilities();
-    const user = SessionStorageService.getItem(Constants.UserInSession);
-    switch (user.role){
-      case 1:
-      case 2:
-      case 3:
-        this._api.getPerson(CookieService.getCookie(Constants.IdSession)).subscribe(response => {
-          switch (response.code) {
-            case 200:
-              this.user = response.item;
-              this._api.getConsultancyBasicData(this.user.id, this.user.refId).subscribe(response => {
-                switch (response.code) {
-                  case 200:
-                    this.consultancyBasicData = response.item;
-                    break;
-                }
-              });
-              break;
-          }
-        });
-        break;
-      default:
-        break;
-    }
-  }
-
-  private changeNotificationsStatus(id: string, status: boolean): void{
+  private changeNotificationsStatus(id: string, status: boolean, index:number): void{
+    debugger;
     if (status) {
       this._dialogService.confirmDialog(
         'Información',
@@ -77,10 +55,10 @@ export class StationListComponent implements OnInit, DoCheck {
       ).afterClosed().subscribe(response =>{
         switch (response.code) {
           case 1:
-            this._api.turnOffNotificationStation(this.user.id, id).subscribe(response=>{
+            this._api.turnOffNotificationStation(CookieService.getCookie(Constants.IdSession), id).subscribe(response=>{
               switch (response.code) {
                 case 200:
-                  this.getStations();
+                  this.notificationActive[index]=false;
                   break;
               }
             });
@@ -88,10 +66,10 @@ export class StationListComponent implements OnInit, DoCheck {
         }
       });
     } else{
-      this._api.turnOnNotificationStation(this.user.id, id).subscribe( response =>{
+      this._api.turnOnNotificationStation(CookieService.getCookie(Constants.IdSession), id).subscribe( response =>{
         switch (response.code) {
           case 200:
-            this.getStations();
+            this.notificationActive[index]=true;
             break;
         }
       })
