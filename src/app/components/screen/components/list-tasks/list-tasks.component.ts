d7f1914f544e78332450d05e80a1defbf8c0b09f
Fercho10 <feruriel00@hotmail.com>
2019-01-15 15:06:17 -0600

    feat: add logical to filters

--- src/app/components/screen/components/list-tasks/list-tasks.component.ts
+++ src/app/components/screen/components/list-tasks/list-tasks.component.ts
@@ -5,6 +5,8 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import {DatepickerService} from '@app/core/components/datepicker/datepicker.service';
+import {TaskFilterService} from '@app/core/components/task-filter/task-filter.service';
 
 @Component({
   selector: 'app-list-tasks',
@@ -12,10 +14,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-tasks.component.scss']
 })
 export class ListTasksComponent implements OnInit {
-
-  constructor() { }
+  public startDate: string = new Date().toLocaleDateString();
+  public endDate: string = new Date().toLocaleDateString();
+  public today: boolean = false;
+  public typeFilter: string[] = ['Todas','Atrasadas','Terminadas','Vencidas','Incidencias'];
+  public filterApply:string;
+  public filter: number = 0;
+  constructor(
+    private _dateService: DatepickerService,
+    private _filterService: TaskFilterService,
+  ) { }
 
   ngOnInit() {
+    if (this.startDate === this.endDate) {
+      this.today = true;
+    }this.filterApply = this.typeFilter[this.filter];
   }
 
+  public dateFilter():void{
+    this._dateService.open().afterClosed().subscribe(response=>{
+      switch (response.code) {
+        case 1:
+          if (response.startDate == response.endDate) {
+            this.today = true;
+          } else {
+            this.today=false;
+            this.startDate = response.startDate;
+            this.endDate = response.endDate;
+          }
+          break;
+      }
+    });
+  }
+
+  public taskFilter():void{
+    this._filterService.open(this.filter).afterClosed().subscribe(response=>{
+      switch (response.code) {
+        case 1:
+          this.filter = response.filter;
+          this.filterApply = this.typeFilter[this.filter];
+          break;
+      }
+    })
+  }
+  public resetFilters():void{
+    this.filter=0;
+    this.filterApply = this.typeFilter[this.filter];
+    this.startDate = new Date().toLocaleDateString();
+    this.endDate = new Date().toLocaleDateString();
+    this.today = true;
+  }
 }
