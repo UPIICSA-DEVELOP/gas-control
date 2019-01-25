e369e1579c9e0966585d378b20236691183afa98
Fercho10 <feruriel00@hotmail.com>
2019-01-25 09:09:49 -0600

    fix: add documentation

--- src/app/components/screen/components/documentation/documentation.component.ts
+++ src/app/components/screen/components/documentation/documentation.component.ts
@@ -8,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 import {UploadFileService} from '@app/core/components/upload-file/upload-file.service';
 import {animate, keyframes, query, stagger, style, transition, trigger} from '@angular/animations';
 import {ActivatedRoute, Router} from '@angular/router';
+import {ApiService} from '@app/core/services/api/api.service';
+import {UploadFileResponse} from '@app/core/components/upload-file/upload-file.component';
+
+interface Document{
+  id?: string;
+  idStation: string;
+  regulationType: number;
+  type: number;
+  file:any;
+}
 
 @Component({
   selector: 'app-documentation',
@@ -37,20 +47,86 @@ import {ActivatedRoute, Router} from '@angular/router';
   ],
   host: {'[@fadeInAnimation]': ''}
 })
+
 export class DocumentationComponent implements OnInit {
   public isASEA: boolean;
   public stationId: string;
+  public docsAsea: any[];
+  public docsCre: any[];
+  public docAseaExist: boolean[];
+  public docCreExist: boolean[];
+  public newDocumentsAsea: FormData[];
+  public newDocumentsCre: FormData[];
   constructor(
     private _uploadFile: UploadFileService,
     private _router: Router,
-    private _activateRoute: ActivatedRoute
+    private _activateRoute: ActivatedRoute,
+    private _api: ApiService
   ) { }
 
   ngOnInit() {
-    if (this._activateRoute.snapshot.queryParams.station) {
-      this.stationId = this._activateRoute.snapshot.queryParams.station
-    }
-
+    this.docAseaExist = [];
+    this.docCreExist = [];
     this.isASEA = true;
+    if (this._activateRoute.snapshot.queryParams.station) {
+      this.stationId = this._activateRoute.snapshot.queryParams.station;
+      this.getDocuments();
+    }
+  }
+
+  private getDocuments():void{
+    this._api.listDocumentByStation(this.stationId, '2').subscribe(response=>{
+      switch (response.code){
+        case 200:
+          this.docsCre = response.items;
+          if (this.docsCre){
+            for (let doc = 0; doc<this.docsCre.length; doc++){
+              for (let type = 1; type<19; type++){
+                if(this.docsCre[doc].type === type){
+                  this.docAseaExist.push(true);
+                }
+              }
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    });
+    this._api.listDocumentByStation(this.stationId, '1').subscribe(response=>{
+      switch (response.code){
+        case 200:
+          this.docsAsea = response.items;
+          if (this.docsAsea){
+            for (let doc = 0; doc<this.docsAsea.length; doc++){
+              for (let type = 1; type<19; type++){
+                if(this.docsAsea[doc].type === type){
+                  this.docAseaExist.push(true);
+                }
+              }
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  public onLoadFile(event: UploadFileResponse, index: number, documentExist: boolean, regulation: number){
+    let doc = new FormData();
+    doc.append('path',this.stationId);
+    doc.append('fileName','Doc1-'+new Date().getTime()+'.pdf');
+    doc.append('file', event.file);
+    switch (regulation){
+      case 1:
+        this.docAseaExist[index] = true;
+        this.newDocumentsAsea.push(doc);
+        break;
+      case 2:
+        this.docCreExist[index] = true;
+        this.newDocumentsCre.push(doc);
+        break;
+    }
   }
 }
