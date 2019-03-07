3e0e675cc59bfa538e70081c5aa5d5aeb7ccdd4a
Fercho10 <feruriel00@hotmail.com>
2019-03-07 12:11:10 -0600

    refactor: modify vrs-report

--- src/app/components/screen/components/list-tasks/components/vrs-report/vrs-report.component.ts
+++ src/app/components/screen/components/list-tasks/components/vrs-report/vrs-report.component.ts
@@ -9,6 +9,9 @@ import {ApiService} from '@app/core/services/api/api.service';
 import {ApiLoaderService} from '@app/core/services/api/api-loader.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {VRSReport} from '@app/core/interfaces/interfaces';
+import {ImageVisorService} from '@app/core/components/image-visor/image-visor.service';
+import {SnackBarService} from '@app/core/services/snackbar/snackbar.service';
+import {UtilitiesService} from '@app/core/utilities/utilities.service';
 
 @Component({
   selector: 'app-vrs-report',
@@ -17,20 +20,31 @@ import {VRSReport} from '@app/core/interfaces/interfaces';
 })
 export class VrsReportComponent implements OnInit {
   private _taskId: string;
+  public task: any;
   @Input() set taskVrsInfo(taskObj: any){
     if(taskObj){
       this._taskId = taskObj.id;
+      this.task = taskObj;
+      this.getVRSReport();
     }
   }
   public load: boolean;
   public vrsForm: FormGroup;
   public vrsReport: VRSReport;
   public date: any[];
+  public taskItems: any[];
+  private _indexTask: number;
   constructor(
     private _api: ApiService,
     private _apiLoader: ApiLoaderService,
-    private _formBuilder: FormBuilder
-  ) { }
+    private _formBuilder: FormBuilder,
+    private _imageVisor: ImageVisorService,
+    private _snackBarService: SnackBarService
+  ) {
+    this.date = [];
+    this.taskItems = [];
+    this._indexTask = 0;
+  }
 
   ngOnInit() {
     this._apiLoader.getProgress().subscribe(load=>{this.load = load});
@@ -50,6 +64,12 @@ export class VrsReportComponent implements OnInit {
       emergencyStop: ['', []],
       observations: ['', []]
     });
+  }
+
+  private resetElements(): void{
+    this.vrsReport = undefined;
+    this.vrsForm.reset();
+    this.vrsForm.disable();
   }
 
   private patchForm(task: any):void{
@@ -78,7 +98,39 @@ export class VrsReportComponent implements OnInit {
       vrsAlarm: this.vrsReport.vrsAlarm,
       emergencyStop: this.vrsReport.emergencyStop,
       observations: this.vrsReport.observations
-    })
+    });
+    this.date = UtilitiesService.convertDate(this.vrsReport.date);
+    this.vrsForm.disable();
+  }
+
+  private getVRSReport(): void{
+    this._api.getTaskInformation(this._taskId, 4).subscribe(response=>{
+      switch (response.code){
+        case 200:
+          if(response.items){
+            this.taskItems = UtilitiesService.sortJSON(response.items,'folio','desc');
+            this._indexTask = 0;
+            this.patchForm(this.taskItems[0]);
+          }else{
+            this.resetElements();
+          }
+          break;
+        default:
+          this.resetElements();
+          break;
+      }
+    });
+  }
+
+  public seeEvidence():void{
+    if(this.task.original.status !== 4){
+      return;
+    }
+    if(this.taskItems[this._indexTask].fileCS){
+      this._imageVisor.open(this.taskItems[this._indexTask].fileCS);
+    }else{
+      this._snackBarService.openSnackBar('Esta tarea no cuenta con evidencia', 'OK',3000);
+    }
   }
 
 }
