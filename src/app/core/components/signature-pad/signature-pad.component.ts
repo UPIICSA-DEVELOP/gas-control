e9814707055ec1cc1ce5a1b657f4012c707b59e9
Fercho10 <feruriel00@hotmail.com>
2019-03-31 19:44:25 -0600

    fix(Signature-Modal): solve error in block cancel button

--- src/app/core/components/signature-pad/signature-pad.component.ts
+++ src/app/core/components/signature-pad/signature-pad.component.ts
@@ -12,6 +12,7 @@ import {UtilitiesService} from '@app/core/utilities/utilities.service';
 import {SnackBarService} from '@app/core/services/snackbar/snackbar.service';
 import {DeviceDetectorService} from '@app/core/services/device-detector/device-detector.service';
 import {LocalStorageService} from '@app/core/services/local-storage/local-storage.service';
+import {Constants} from '@app/core/constants.core';
 
 @Component({
   selector: 'app-signature-pad',
@@ -31,7 +32,7 @@ export class SignaturePadComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.disable = LocalStorageService.getItem('notSign');
+    this.disable = LocalStorageService.getItem(Constants.NotSignature);
     if (isPlatformBrowser(this._platformId)) {
       const container = this._document.getElementById('pad');
       this.canvas = container.querySelector('canvas');
