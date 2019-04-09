dba5df2a8f84235c0b948bdc68ad2edd46bb6924
Fercho10 <feruriel00@hotmail.com>
2019-04-09 15:00:11 -0500

    fix(SASISOPA): add view pdf and text errors

--- src/app/components/screen/components/sasisopa/sasisopa.component.ts
+++ src/app/components/screen/components/sasisopa/sasisopa.component.ts
@@ -43,7 +43,9 @@ export class SasisopaComponent implements OnInit, OnDestroy {
   public generate: boolean;
   public isAvailable: boolean;
   private dateGeneration: string[];
-  private _sasisopaDocs: any[];
+  public sasisopaDocs: any[];
+  public docFile: any[];
+  public errors: boolean[];
   private _subscriptionLoader: Subscription;
   private _token: string;
   private _change: boolean;
@@ -60,6 +62,9 @@ export class SasisopaComponent implements OnInit, OnDestroy {
     private _dialogService: DialogService,
     private _snackBarService: SnackBarService
   ) {
+    this.docFile = [null, null, null, null, null, null, null, null, null, null, null, null];
+    this.sasisopaDocs = [null, null, null, null, null, null, null, null, null, null, null, null];
+    this.errors = [false, false, false, false,  false, false, false, false];
     this.dateGeneration = [];
     this.zones = Constants.Zones;
     this.frequency = Constants.Frequency;
@@ -95,6 +100,7 @@ export class SasisopaComponent implements OnInit, OnDestroy {
 
   public changeElementOnView(type: number): void{
     this.elementInView = type;
+    this.resetErrors();
   }
 
   public close():void{
@@ -137,7 +143,20 @@ export class SasisopaComponent implements OnInit, OnDestroy {
     }else{
       url = this._data.utils.sasisopaTemplates[0].fileCS.thumbnail;
     }
-    this._pdf.open({file: url, url: HashService.set("123456$#@$^@1ERF", url), notIsUrl: false, hideDownload: true})
+    const user = LocalStorageService.getItem(Constants.UserInSession);
+    switch (user.role){
+      case 1:
+      case 2:
+      case 7:
+        this._pdf.open({urlOrFile: HashService.set("123456$#@$^@1ERF", url)});
+        break;
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        this._pdf.open({urlOrFile: HashService.set("123456$#@$^@1ERF", url), hideDownload: true});
+        break;
+    }
   }
 
   public addRemoveBrigadeElem(isAdd: boolean, index?: number): void{
@@ -190,12 +209,14 @@ export class SasisopaComponent implements OnInit, OnDestroy {
     form.append('fileName', 'SASISOPA'+annexed+'-'+new Date().getTime()+'.pdf');
     form.append('file', ev.file);
     this._forms[type-1] = form;
+    this.docFile[type-1] = ev.file;
   }
 
   private uploadFile(annexed: number, index: number):void{
     this._uploadFileService.upload(this._forms[index-1]).subscribe(response => {
       if(response){
         this._forms[index-1] = undefined;
+        this.docFile[index-1] = undefined;
         const doc: SasisopaDocument = {
           annexed: annexed,
           file: response.item,
@@ -212,6 +233,7 @@ export class SasisopaComponent implements OnInit, OnDestroy {
       switch(response.code){
         case 200:
           this._change = false;
+          this.sasisopaDocs[element.type - 1] = element;
           this.saveChanges(element.annexed);
           break;
         default:
@@ -350,7 +372,14 @@ export class SasisopaComponent implements OnInit, OnDestroy {
             this.getStationTasks(date.timeStamp);
           }
           if(response.item.sasisopaDocuments){
-            this._sasisopaDocs = UtilitiesService.sortJSON(response.item.sasisopaDocuments, 'type', 'asc');
+            const sasisopaDocs = UtilitiesService.sortJSON(response.item.sasisopaDocuments, 'type', 'asc');
+            for(let i = 0; i < this.sasisopaDocs.length; i++){
+              for(let j = 0; j < sasisopaDocs.length; j++){
+                if(sasisopaDocs[j].type === i + 1){
+                  this.sasisopaDocs[i] = sasisopaDocs[j];
+                }
+              }
+            }
           }
           if(response.item.fullSasisopa){
             this.generate = true;
@@ -435,22 +464,41 @@ export class SasisopaComponent implements OnInit, OnDestroy {
 
   public generateSasisopa():void{
     let error = false;
-    if(this._sasisopaDocs.length < 12){
-      error = true;
-    }
-    if(this.brigade.length === 0){
-      error = true;
-    }
-    if(!this.date){
-      error = true;
-    }
     for(let i = 0; i<this.listCollaborators.length; i++){
       if(!this.listCollaborators[i].signature || !this.listCollaborators[i].signature.thumbnail){
+        this.errors[0] = true;
         error = true;
       }
     }
+    if(!this.sasisopaDocs[0] || !this.sasisopaDocs[1]){
+      this.errors[1] = true;
+      error = true;
+    }
+    if(!this.sasisopaDocs[2] || !this.sasisopaDocs[3] || this.brigade.length === 0){
+      this.errors[2] = true;
+      error = true;
+    }
+    if(!this.sasisopaDocs[4]){
+      this.errors[3] = true;
+      error = true;
+    }
+    if(!this.date){
+      this.errors[4] = true;
+      error = true;
+    }
+    if(!this.sasisopaDocs[5]){
+      this.errors[5] = true;
+      error = true;
+    }
+    if(!this.sasisopaDocs[6] || !this.sasisopaDocs[7]){
+      this.errors[6] = true;
+      error = true;
+    }
+    if(!this.sasisopaDocs[11]){
+      this.errors[7] = true;
+      error = true;
+    }
     if(error){
-      this._snackBarService.openSnackBar('Por favor, complete la información para generar el SASISOPA','OK',3000);
       return;
     }else{
       this._api.getFullPDF(this.station.id, false).subscribe(response =>{
@@ -478,15 +526,42 @@ export class SasisopaComponent implements OnInit, OnDestroy {
         case 1:
         case 2:
         case 7:
-          this._pdf.open({file: response, url: HashService.set("123456$#@$^@1ERF", response), notIsUrl: true });
+          this._pdf.open({ urlOrFile: response, });
           break;
         case 3:
         case 4:
         case 5:
         case 6:
-          this._pdf.open({file: response, url: HashService.set("123456$#@$^@1ERF", response), notIsUrl: true, hideDownload: true });
+          this._pdf.open({urlOrFile: response, hideDownload: true });
           break;
       }
     });
+  }
+
+  private resetErrors(): void{
+    for(let i = 0; i < 8; i++){
+      this.errors[i] = false;
+    }
+  }
+
+  public seeDocuments(type: number): void{
+    const user = LocalStorageService.getItem(Constants.UserInSession);
+    if(!this.docFile[type-1] && !this.sasisopaDocs[type-1]){
+      this._snackBarService.openSnackBar('No se ha escaneado el documento', 'OK', 3000);
+      return;
+    }
+    switch (user.role){
+      case 1:
+      case 2:
+      case 7:
+        this._pdf.open({urlOrFile: this.docFile[type-1] ? this.docFile[type-1]: HashService.set("123456$#@$^@1ERF",this.sasisopaDocs[type-1].file.thumbnail)});
+         break;
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        this._pdf.open({urlOrFile: this.docFile[type-1] ? this.docFile[type-1]: HashService.set("123456$#@$^@1ERF",this.sasisopaDocs[type-1].file.thumbnail), hideDownload: true});
+        break;
+    }
   }
 }
