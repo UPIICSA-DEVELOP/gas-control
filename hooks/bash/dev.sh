fb2d40d191a5f0279a178630a9ed0f421a59f82c
Alejandro López A <program.alopez@gmail.com>
2019-04-12 08:50:51 -0500

    fix: Remove more 3 backups

--- hooks/bash/dev.sh
+++ hooks/bash/dev.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo [START DEPLOY TO DEVELOP `date '+%Y-%m-%d %H:%M:%S'`]
-sudo zip -r /var/www/html/app.inspector.com/deploy/backups/backup.dev-`date '+%Y-%m-%d-%H:%M:%S'`.zip /var/www/html/app.inspector.com/develop/ --exclude=/var/www/html/app.inspector.com/develop/node_modules**
+x="ls /var/www/html/app.inspector.com/deploy/backups/dev/ -1 | wc -l"
+y=$(eval "$x")
+if [ $y -gt 3 ]
+then
+  echo More than 3 backups
+  sudo rm -rf /var/www/html/app.inspector.com/deploy/backups/dev/*
+else
+  echo Less than 3 backups
+fi
+sudo zip -r /var/www/html/app.inspector.com/deploy/backups/dev/backup.dev-`date '+%Y-%m-%d-%H:%M:%S'`.zip /var/www/html/app.inspector.com/develop/ --exclude=/var/www/html/app.inspector.com/develop/node_modules**
 sudo rm -rf /var/www/html/app.inspector.com/develop/*
 sudo rm -rf /var/www/html/app.inspector.com/develop/.well-known/*
 sudo unzip /var/www/html/app.inspector.com/deploy/bundle.zip -d /var/www/html/app.inspector.com/develop
--- hooks/bash/prod.sh
+++ hooks/bash/prod.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo [START DEPLOY TO PRODUCTION `date '+%Y-%m-%d %H:%M:%S'`]
-sudo zip -r /var/www/html/app.inspector.com/deploy/backups/backup.prod-`date '+%Y-%m-%d-%H:%M:%S'`.zip /var/www/html/app.inspector.com/production/ --exclude=/var/www/html/app.inspector.com/production/node_modules**
+x="ls /var/www/html/app.inspector.com/deploy/backups/prod/ -1 | wc -l"
+y=$(eval "$x")
+if [ $y -gt 3 ]
+then
+  echo More than 3 backups
+  sudo rm -rf /var/www/html/app.inspector.com/deploy/backups/prod/*
+else
+  echo Less than 3 backups
+fi
+sudo zip -r /var/www/html/app.inspector.com/deploy/backups/prod/backup.prod-`date '+%Y-%m-%d-%H:%M:%S'`.zip /var/www/html/app.inspector.com/production/ --exclude=/var/www/html/app.inspector.com/production/node_modules**
 sudo rm -rf /var/www/html/app.inspector.com/production/*
 sudo rm -rf /var/www/html/app.inspector.com/production/.well-known/*
 sudo unzip /var/www/html/app.inspector.com/deploy/bundle.zip -d /var/www/html/app.inspector.com/production
