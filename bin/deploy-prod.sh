fecac5431c9e908cbad7cf934e421364d65a9655
Alejandro López A <program.alopez@gmail.com>
2019-06-04 11:00:33 -0500

    fix(bin): Remove files unnecessary

--- bin/deploy-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-gulp zip --file dist
-node hooks/scp --filePath dist/bundle.zip --destination /var/www/html/app.inspector.com/deploy
-node hooks/remotely --env prod
-exit 0
--- bin/deploy-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-gulp zip --file hooks/bash
-node hooks/scp --filePath hooks/bash/bundle.zip --destination /var/www/html/app.inspector.com/deploy/bash
-node hooks/remotely --setup
-exit 0
