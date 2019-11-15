4a44126e86147620bbe212b43ad436e14bb32636
Israel Olvera <skrigueztep@gmail.com>
2019-11-15 10:34:28 -0600

    feat: EntityCollectionResponse class added

---
+++ src/app/utils/class/entity-collection-response.ts
@@ -0,0 +1,9 @@
+import {DefaultResponse} from '@app/utils/interfaces/default-response';
+import {HttpResponseCodes} from '@app/utils/enums/http-response-codes';
+
+export class EntityCollectionResponse<T> implements DefaultResponse{
+  code: HttpResponseCodes;
+  description: string;
+  items: T[];
+  nextPageToken: string;
+}
