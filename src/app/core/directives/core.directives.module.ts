3a03c507dc366608f84f0e9736e0f9e2df310759
Israel Olvera <skrigueztep@gmail.com>
2019-11-20 12:45:43 -0600

    style: core directives module formatted based-TSLint rules

--- src/app/core/directives/core.directives.module.ts
+++ src/app/core/directives/core.directives.module.ts
@@ -5,16 +5,15 @@
  *
  */
 
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [
-  ],
-  exports: [
-  ]
+  declarations: [],
+  exports: []
 })
-export class CoreDirectivesModule { }
+export class CoreDirectivesModule {
+}
