510c5758a80af6fc942148aed3b16b9ccd2fedde
Israel Olvera <skrigueztep@gmail.com>
2019-11-20 16:07:11 -0600

    fix: arguments type changed

--- src/app/shared/pipes/format-folio/format-folio.pipe.ts
+++ src/app/shared/pipes/format-folio/format-folio.pipe.ts
@@ -5,30 +5,32 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 export class FormatFolioPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: number): string {
+    let result = '';
     try {
-      value = value.toString();
-      switch (value.length) {
+      switch (value.toString().length) {
         case 1:
-          value = '00000' + value;
+          result = `00000${value}`;
           break;
         case 2:
-          value = '0000' + value;
+          result = `0000${value}`;
           break;
         case 3:
-          value = '000' + value;
+          result = `000${value}`;
           break;
         case 4:
-          value = '00' + value;
+          result = `00${value}`;
           break;
         case 5:
-          value = '0' + value;
+          result = `0${value}`;
+          break;
+        default:
+          result = value.toString();
           break;
       }
-      return value;
+      return result;
     } catch (e) {
-      console.error(e);
-      return null;
+      return result;
     }
   }
 
