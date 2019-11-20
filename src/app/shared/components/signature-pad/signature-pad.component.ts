1bf8900d5095525080797277f3e92d6bf45329e2
Israel Olvera <skrigueztep@gmail.com>
2019-11-20 14:23:44 -0600

    style: signature component & service formatted based-TSLint rules

--- src/app/shared/components/signature-pad/signature-pad.component.ts
+++ src/app/shared/components/signature-pad/signature-pad.component.ts
@@ -24,13 +24,15 @@ export class SignaturePadComponent implements OnInit {
   public canvas: any;
   public pad: any;
   public disable: boolean;
+
   constructor(
     @Inject(PLATFORM_ID) private _platformId,
     @Inject(DOCUMENT) private _document: Document,
     private _dialogRef: MatDialogRef<SignaturePadComponent>,
     private _snackBarService: SnackBarService,
     private _deviceDetector: DeviceDetectorService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.disable = LocalStorageService.getItem(Constants.NotSignature);
@@ -38,34 +40,34 @@ export class SignaturePadComponent implements OnInit {
       const container = this._document.getElementById('pad');
       this.canvas = container.querySelector('canvas');
       this.resizeCanvas();
-      this.pad = new SignaturePad(this.canvas,{
+      this.pad = new SignaturePad(this.canvas, {
         penColor: '#000000',
         backgroundColor: '#FFFFFF'
       });
     }
   }
 
-  public clearCanvas(): void{
+  public clearCanvas(): void {
     this.pad.clear();
   }
 
-  public closeCancel():void{
+  public closeCancel(): void {
     this._dialogRef.close({code: -1});
   }
 
-  public closeAccept():void{
-    if (this.pad.isEmpty()){
-      this._snackBarService.openSnackBar('Por favor registre su firma','OK',2000);
+  public closeAccept(): void {
+    if (this.pad.isEmpty()) {
+      this._snackBarService.openSnackBar('Por favor registre su firma', 'OK', 2000);
       return;
     }
-    let data = this.pad.toDataURL('image/png');
+    const data = this.pad.toDataURL('image/png');
     this._dialogRef.close({code: 1, blob: UtilitiesService.base64ToBlob(data), base64: data});
   }
 
-  public resizeCanvas():void{
-    const percent = this._deviceDetector.isMobileDevice()?.80:.40;
+  public resizeCanvas(): void {
+    const percent = this._deviceDetector.isMobileDevice() ? .80 : .40;
     const width = this._document.body.clientWidth;
-    this.canvas.width = ((width * percent) - 36 );
+    this.canvas.width = ((width * percent) - 36);
     this.canvas.height = (this.canvas.width / 2);
   }
 }
--- src/app/shared/components/signature-pad/signature-pad.service.ts
+++ src/app/shared/components/signature-pad/signature-pad.service.ts
@@ -4,7 +4,7 @@
  * Proprietary and confidential
  */
 
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material';
 import {SignaturePadComponent} from 'app/shared/components/signature-pad/signature-pad.component';
 
@@ -13,9 +13,10 @@ export class SignaturePadService {
 
   constructor(
     private _dialog: MatDialog
-  ) { }
+  ) {
+  }
 
-  public open(): MatDialogRef<SignaturePadComponent>{
-    return this._dialog.open(SignaturePadComponent,{panelClass:'signature-panel', disableClose: true});
+  public open(): MatDialogRef<SignaturePadComponent> {
+    return this._dialog.open(SignaturePadComponent, {panelClass: 'signature-panel', disableClose: true});
   }
 }
