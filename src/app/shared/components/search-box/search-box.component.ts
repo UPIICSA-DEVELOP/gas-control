a441f32a135aff9bc5226a1ce43da1c96a00ee47
Israel Olvera <skrigueztep@gmail.com>
2019-11-20 14:22:09 -0600

    style: search-box component formatted based-TSLint rules

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -2,13 +2,13 @@ import {Component, ElementRef, EventEmitter, Input, NgZone, OnInit, Output, View
 import {FormControl} from '@angular/forms';
 import {MapsAPILoader} from '@agm/core';
 
-export interface SearchBoxResult{
+export interface SearchBoxResult {
   lat: number;
   lng: number;
   address?: string;
 }
 
-export interface SearchBoxStyles{
+export interface SearchBoxStyles {
   padding: string;
   paddingLeft?: string;
   paddingTop?: string;
@@ -27,7 +27,7 @@ export interface SearchBoxStyles{
 })
 export class SearchBoxCoreComponent implements OnInit {
 
-  @ViewChild('searchBoxCore', { static: true }) searchBoxCore: ElementRef;
+  @ViewChild('searchBoxCore', {static: true}) searchBoxCore: ElementRef;
   @Output() onChange: EventEmitter<SearchBoxResult>;
   @Output() inputSearchBox: EventEmitter<ElementRef>;
   @Input() styles: SearchBoxStyles;
@@ -35,6 +35,7 @@ export class SearchBoxCoreComponent implements OnInit {
   @Input() placeHolder: string;
   @Input() disabled: boolean;
   public searchControl: FormControl;
+
   constructor(
     private mapsAPILoader: MapsAPILoader,
     private _ngZone: NgZone
@@ -45,47 +46,47 @@ export class SearchBoxCoreComponent implements OnInit {
   }
 
   ngOnInit() {
-   if(this.styles){
-     this.setConfig(this.styles);
-   }
+    if (this.styles) {
+      this.setConfig(this.styles);
+    }
     this.init();
   }
 
-  private init(): void{
+  private init(): void {
     this.inputSearchBox.emit(this.searchBoxCore);
     this.mapsAPILoader.load().then(() => {
-      let autocomplete = new google.maps.places.Autocomplete(this.searchBoxCore.nativeElement, {
-        types: ["address"]
+      const autocomplete = new google.maps.places.Autocomplete(this.searchBoxCore.nativeElement, {
+        types: ['address']
       });
-      autocomplete.addListener("place_changed", () => {
+      autocomplete.addListener('place_changed', () => {
         this._ngZone.run(() => {
-          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
           if (place.geometry === undefined || place.geometry === null) {
             return;
           }
-          this.onChange.emit({lat: place.geometry.location.lat(),  lng:  place.geometry.location.lng(), address: place.formatted_address});
+          this.onChange.emit({lat: place.geometry.location.lat(), lng: place.geometry.location.lng(), address: place.formatted_address});
         });
       });
     });
   }
 
-  private setConfig(config: SearchBoxStyles): void{
-    if(config.padding){
+  private setConfig(config: SearchBoxStyles): void {
+    if (config.padding) {
       this.searchBoxCore.nativeElement.style.padding = config.padding;
     }
-    if(config.paddingLeft){
-       this.searchBoxCore.nativeElement.style.paddingLeft = config.paddingLeft;
+    if (config.paddingLeft) {
+      this.searchBoxCore.nativeElement.style.paddingLeft = config.paddingLeft;
     }
-    if(config.paddingTop){
-       this.searchBoxCore.nativeElement.style.paddingTop = config.paddingTop;
+    if (config.paddingTop) {
+      this.searchBoxCore.nativeElement.style.paddingTop = config.paddingTop;
     }
-    if(config.paddingRight){
-       this.searchBoxCore.nativeElement.style.paddingRight = config.paddingRight;
+    if (config.paddingRight) {
+      this.searchBoxCore.nativeElement.style.paddingRight = config.paddingRight;
     }
-    if(config.paddingBottom){
-       this.searchBoxCore.nativeElement.style.paddingBottom = config.paddingBottom;
+    if (config.paddingBottom) {
+      this.searchBoxCore.nativeElement.style.paddingBottom = config.paddingBottom;
     }
-    if(this.styles.width){
+    if (this.styles.width) {
       this.searchBoxCore.nativeElement.style.width = this.styles.width;
     }
   }
