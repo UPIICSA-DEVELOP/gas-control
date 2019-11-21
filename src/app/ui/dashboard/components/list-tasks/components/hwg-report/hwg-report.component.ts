7a55f3115e87e159bb7febc6811698b659173a9e
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:18:56 -0600

    style: hwg-report component formatted based-TSLint rules and typed

--- src/app/ui/dashboard/components/list-tasks/components/hwg-report/hwg-report.component.ts
+++ src/app/ui/dashboard/components/list-tasks/components/hwg-report/hwg-report.component.ts
@@ -19,21 +19,24 @@ import {HWGReport} from '@app/utils/interfaces/reports/hwg-report';
 })
 export class HwgReportComponent implements OnInit, OnDestroy {
   public report: HWGReport;
-  @Input() set taskHwgInfo(reportObj: HWGReport){
-    if(reportObj){
+
+  @Input() set taskHwgInfo(reportObj: HWGReport) {
+    if (reportObj) {
       this.initHwgForm();
       this.report = reportObj;
       this.getHWGReport();
-    }else{
+    } else {
       this.initHwgForm();
       this.resetElements();
     }
   }
-  @Input() set validate(ban: boolean){
-    if(ban){
+
+  @Input() set validate(ban: boolean) {
+    if (ban) {
       this._document.getElementById('hidden-button').click();
     }
   }
+
   @Output() formInformation: EventEmitter<any>;
   public load: boolean;
   public hwgReport: HWGReport;
@@ -42,6 +45,7 @@ export class HwgReportComponent implements OnInit, OnDestroy {
   public editable: boolean;
   private _subscriptionShared: Subscription;
   private _copyTask: HWGReport;
+
   constructor(
     @Inject(DOCUMENT) private _document: Document,
     private _formBuilder: FormBuilder,
@@ -56,21 +60,19 @@ export class HwgReportComponent implements OnInit, OnDestroy {
     this.getNotifications();
   }
 
-  ngOnDestroy():void{
+  ngOnDestroy(): void {
     this._subscriptionShared.unsubscribe();
   }
 
-  private getNotifications():void{
-    this._subscriptionShared = this._sharedService.getNotifications().subscribe(response=>{
-      switch (response.type){
-        case SharedTypeNotification.HwgActive:
-          this.startEditComplementReport(response.value);
-          break;
+  private getNotifications(): void {
+    this._subscriptionShared = this._sharedService.getNotifications().subscribe(response => {
+      if (response.type === SharedTypeNotification.HwgActive) {
+        this.startEditComplementReport(response.value);
       }
-    })
+    });
   }
 
-  private initHwgForm():void{
+  private initHwgForm(): void {
     this.hwgForm = this._formBuilder.group({
       area: ['', [Validators.required]],
       waste: ['', [Validators.required]],
@@ -85,13 +87,13 @@ export class HwgReportComponent implements OnInit, OnDestroy {
     });
   }
 
-  private resetElements():void{
+  private resetElements(): void {
     this.hwgReport = undefined;
     this.hwgForm.reset();
     this.hwgForm.disable();
   }
 
-  private patchForm(report: HWGReport):void{
+  private patchForm(report: HWGReport): void {
     this.hwgReport = {
       area: report.area || null,
       corrosive: report.corrosive || null,
@@ -119,34 +121,34 @@ export class HwgReportComponent implements OnInit, OnDestroy {
     this.hwgForm.disable();
   }
 
-  private getHWGReport():void{
-    if(this.report){
+  private getHWGReport(): void {
+    if (this.report) {
       this.patchForm(this.report);
-    }else{
+    } else {
       this.resetElements();
     }
   }
 
-  private startEditComplementReport(isNewLoad: boolean):void{
+  private startEditComplementReport(isNewLoad: boolean): void {
     this.editable = true;
-    if(!isNewLoad){
-      this._copyTask = this.hwgReport
+    if (!isNewLoad) {
+      this._copyTask = this.hwgReport;
     }
     this.hwgForm.enable();
   }
 
-  public validateForm(value: any): void{
-    if(!value.corrosive && !value.reactive && !value.explosive && !value.toxic && !value.flammable){
+  public validateForm(value: any): void {
+    if (!value.corrosive && !value.reactive && !value.explosive && !value.toxic && !value.flammable) {
       this.error = true;
     }
-    if(this.error || this.hwgForm.invalid){
+    if (this.error || this.hwgForm.invalid) {
       this.formInformation.emit({valid: false, value: undefined});
-    }else{
+    } else {
       this.formInformation.emit({valid: true, value: value});
     }
   }
 
-  public changeFeatures(ev: any):void{
+  public changeFeatures(): void {
     this.error = false;
   }
 
