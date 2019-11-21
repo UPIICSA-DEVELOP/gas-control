936c7231659a75ac8f9eddc3580dee577e030599
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:29:09 -0600

    style: add-collaborator component formatted based-TSLint rules and typed

--- src/app/ui/dashboard/pages/add-collaborator/add-collaborator.component.ts
+++ src/app/ui/dashboard/pages/add-collaborator/add-collaborator.component.ts
@@ -4,12 +4,10 @@
  * Proprietary and confidential
  */
 
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
+import {Component, ElementRef, HostBinding, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {ApiService} from '@app/core/services/api/api.service';
 import {SnackBarService} from '@app/core/services/snackbar/snackbar.service';
-import {SharedService} from '@app/core/services/shared/shared.service';
-import {SharedTypeNotification} from '@app/core/services/shared/shared.service';
+import {SharedService, SharedTypeNotification} from '@app/core/services/shared/shared.service';
 import {LocalStorageService} from '@app/core/services/local-storage/local-storage.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
@@ -24,28 +22,20 @@ import {PdfVisorService} from '@app/shared/components/pdf-visor/pdf-visor.servic
 import {UploadFileResponse} from '@app/shared/components/upload-file/upload-file.component';
 import {Person} from '@app/utils/interfaces/person';
 import {PersonInformation} from '@app/utils/interfaces/person-information';
+import {HttpResponseCodes} from '@app/utils/enums/http-response-codes';
+import {ANIMATION} from '@app/ui/dashboard/pages/add-collaborator/animation';
 
 @Component({
   selector: 'app-add-collaborator',
   templateUrl: './add-collaborator.component.html',
   styleUrls: ['./add-collaborator.component.scss'],
-  animations: [
-    trigger('fadeInAnimation', [
-      transition(':enter', [
-        style({ right: '-100%' }),
-        animate('.40s ease-out', style({ right: '0'  }))
-      ]),
-      transition(':leave', [
-        style({ right: '0'}),
-        animate('.40s ease-in', style({ right: '-100%' }))
-      ])
-    ])
-  ],
-  host: {'[@fadeInAnimation]': ''},
+  animations: [ANIMATION],
   encapsulation: ViewEncapsulation.None
 })
 export class AddCollaboratorComponent implements OnInit, OnDestroy {
-  @ViewChild('phoneNumber', { static: true }) private _phoneNumberInput: ElementRef;
+  @HostBinding('@fadeInAnimation')
+
+  @ViewChild('phoneNumber', {static: true}) private _phoneNumberInput: ElementRef;
   public signature: any;
   public profileImage: any;
   public file: any;
@@ -63,14 +53,15 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
   public roleType: string[];
   public newFile: boolean;
   public bloodType: string;
-  private _formImage:FormData;
+  private _formImage: FormData;
   private _formSignature: FormData;
   private _formFile: FormData;
   private _refId: string;
   public changes: boolean;
   private _subscriptionLoader: Subscription;
+
   constructor(
-    private _api:ApiService,
+    private _api: ApiService,
     private _apiLoader: LoaderService,
     private _dialogService: DialogService,
     private _countryCodeService: CountryCodeService,
@@ -83,7 +74,7 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
     private _sharedService: SharedService,
     private _activateRoute: ActivatedRoute
   ) {
-    this.changes=false;
+    this.changes = false;
     this.roleType = Constants.roles;
     this.bloodGroup = Constants.bloodGroup;
     this.addImage = false;
@@ -96,53 +87,53 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
     if (this._activateRoute.snapshot.queryParams.stationId) {
       this._refId = this._activateRoute.snapshot.queryParams.stationId;
     }
-    this.user= LocalStorageService.getItem(Constants.UserInSession);
-    this._subscriptionLoader = this._apiLoader.getProgress().subscribe(load => {this.load = load; });
+    this.user = LocalStorageService.getItem(Constants.UserInSession);
+    this._subscriptionLoader = this._apiLoader.getProgress().subscribe(load => {
+      this.load = load;
+    });
     this.initForm();
   }
 
-  ngOnDestroy(): void{
+  ngOnDestroy(): void {
     this._subscriptionLoader.unsubscribe();
   }
 
-  public close():void{
-    if (this.changes){
+  public close(): void {
+    if (this.changes) {
       this._dialogService.confirmDialog('¿Desea salir sin guardar cambios?',
         '',
         'ACEPTAR',
-        'CANCELAR').afterClosed().subscribe(response=>{
-        switch (response.code) {
-          case 1:
-            this._router.navigate(['/home']).then();
-            break;
+        'CANCELAR').afterClosed().subscribe(response => {
+        if (response.code === 1) {
+          this._router.navigate(['/home']).then();
         }
       });
-    }else{
+    } else {
       this._router.navigate(['/home']).then();
     }
   }
 
-  public onLoadImage(event: UploadFileResponse):void{
-    this.changes=true;
+  public onLoadImage(event: UploadFileResponse): void {
+    this.changes = true;
     this.blobImageProfile = event.url;
     this.addImage = true;
     this._formImage = new FormData();
-    this._formImage.append('path','');
-    this._formImage.append('filename','profileImage-'+this.user.refId+'-'+new Date().getTime()+'.png');
+    this._formImage.append('path', '');
+    this._formImage.append('filename', 'profileImage-' + this.user.refId + '-' + new Date().getTime() + '.png');
     this._formImage.append('isImage', 'true');
     this._formImage.append('file', event.blob);
   }
 
-  public  onRemoveImage(): void{
-    this.changes=true;
+  public onRemoveImage(): void {
+    this.changes = true;
     this.blobImageProfile = '';
     this.addImage = false;
   }
 
-  public selectCountryCode():void{
-    this._countryCodeService.openDialog().afterClosed().subscribe(response=>{
+  public selectCountryCode(): void {
+    this._countryCodeService.openDialog().afterClosed().subscribe(response => {
       if (response) {
-        this.changes=true;
+        this.changes = true;
         this.country = response.iso;
         this.newPerson.patchValue({
           country: response.name,
@@ -153,36 +144,34 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
     });
   }
 
-  public onLoadFile(event: UploadFileResponse): void{
-    this.changes=true;
+  public onLoadFile(event: UploadFileResponse): void {
+    this.changes = true;
     this.newFile = true;
     this.file = event.file;
     this._formFile = new FormData();
     this._formFile.append('path', '');
-    this._formFile.append('fileName', 'benzene-'+this.user.refId+'-'+new Date().getTime()+'.pdf');
+    this._formFile.append('fileName', 'benzene-' + this.user.refId + '-' + new Date().getTime() + '.pdf');
     this._formFile.append('file', event.file);
   }
 
-  public addSignature(): void{
-    this._signatureService.open().afterClosed().subscribe(response=>{
-      switch (response.code) {
-        case 1:
-          this.changes=true;
-          this.blobSignature = response.base64;
-          this.addSign = true;
-          this._formSignature = new FormData();
-          this._formSignature.append('path', '');
-          this._formSignature.append('fileName', 'signature-'+this.user.refId+'-'+new Date().getTime()+'.png');
-          this._formSignature.append('isImage', 'true');
-          this._formSignature.append('file', response.blob);
-          break;
+  public addSignature(): void {
+    this._signatureService.open().afterClosed().subscribe(response => {
+      if (response.code === 1) {
+        this.changes = true;
+        this.blobSignature = response.base64;
+        this.addSign = true;
+        this._formSignature = new FormData();
+        this._formSignature.append('path', '');
+        this._formSignature.append('fileName', 'signature-' + this.user.refId + '-' + new Date().getTime() + '.png');
+        this._formSignature.append('isImage', 'true');
+        this._formSignature.append('file', response.blob);
       }
-    })
+    });
   }
 
-  public uploadGenericFile(type:number): void{
+  public uploadGenericFile(type: number): void {
     let form: FormData;
-    switch (type){
+    switch (type) {
       case 1:
         form = this._formImage;
         this.addImage = false;
@@ -196,9 +185,9 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
         this.newFile = false;
         break;
     }
-    this._uploadFileService.upload(form).subscribe(response=>{
-      if(response){
-        switch (type){
+    this._uploadFileService.upload(form).subscribe(response => {
+      if (response) {
+        switch (type) {
           case 1:
             this.profileImage = {
               blobName: response.item.blobName || '',
@@ -223,55 +212,55 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
     });
   }
 
-  public validateForm(data: any):void{
-    if (this.newPerson.invalid){
+  public validateForm(data: any): void {
+    if (this.newPerson.invalid) {
       return;
     }
-    if (this.newFile){
+    if (this.newFile) {
       this.uploadGenericFile(3);
       return;
     }
-    if (this.addImage){
+    if (this.addImage) {
       this.uploadGenericFile(1);
       return;
     }
-    if (this.addSign){
+    if (this.addSign) {
       this.uploadGenericFile(2);
       return;
     }
-    this.createCollaborator(data)
+    this.createCollaborator(data);
   }
 
-  private initForm():void{
+  private initForm(): void {
     this.newPerson = this._formBuilder.group({
-      name:['',[Validators.required]],
-      lastName:['',[Validators.required]],
-      email:['',[Validators.required, Validators.email]],
-      country:['México',[Validators.required]],
-      code:['52',[]],
-      phoneNumber:['',[Validators.required, Validators.minLength(8), Validators.maxLength(13)]],
-      role:['',[Validators.required]],
-      jobTitle:['',[Validators.required]],
-      website:['',[Validators.pattern('[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$')]],
-      ssn:['',[]],
-      bloodType:['',[]],
-      contactName:['',[]],
-      contactPhoneNumber:['',[Validators.minLength(8), Validators.maxLength(13)]],
-      contactKinship:['',[]]
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      country: ['México', [Validators.required]],
+      code: ['52', []],
+      phoneNumber: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(13)]],
+      role: ['', [Validators.required]],
+      jobTitle: ['', [Validators.required]],
+      website: ['', [Validators.pattern('[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$')]],
+      ssn: ['', []],
+      bloodType: ['', []],
+      contactName: ['', []],
+      contactPhoneNumber: ['', [Validators.minLength(8), Validators.maxLength(13)]],
+      contactKinship: ['', []]
     });
     this.country = 'MX';
     this.detectChange();
   }
 
-  private detectChange(): void{
-    this.newPerson.valueChanges.subscribe( value => {
+  private detectChange(): void {
+    this.newPerson.valueChanges.subscribe(() => {
       this.changes = true;
-    })
+    });
   }
 
-  private createCollaborator(data:any):void{
-    data.code = data.code.replace('+','');
-    let person: Person ={
+  private createCollaborator(data: any): void {
+    data.code = data.code.replace('+', '');
+    const person: Person = {
       active: false,
       name: data.name,
       lastName: data.lastName,
@@ -280,105 +269,94 @@ export class AddCollaboratorComponent implements OnInit, OnDestroy {
       country: this.country,
       countryCode: data.code,
       role: data.role,
-      website: (data.website?this.protocol + data.website:undefined),
+      website: (data.website ? this.protocol + data.website : undefined),
       jobTitle: data.jobTitle,
       phoneNumber: data.phoneNumber,
-      profileImage:(this.profileImage?this.profileImage:undefined),
-      signature:(this.signature?this.signature:undefined),
+      profileImage: (this.profileImage ? this.profileImage : undefined),
+      signature: (this.signature ? this.signature : undefined),
       bCard: undefined
     };
-    let personInformation: PersonInformation = {
+    const personInformation: PersonInformation = {
       id: '',
-      benzene: (this.file?this.file:undefined),
-      bloodType: (data.bloodType? data.bloodType:undefined),
-      ssn: (data.ssn? data.ssn:undefined),
-      contactName: (data.contactName? data.contactName:undefined),
-      concatcPhone:(data.contactPhoneNumber? data.contactPhoneNumber:undefined),
-      contactKinship: (data.contactKinship?data.contactKinship:undefined)
+      benzene: (this.file ? this.file : undefined),
+      bloodType: (data.bloodType ? data.bloodType : undefined),
+      ssn: (data.ssn ? data.ssn : undefined),
+      contactName: (data.contactName ? data.contactName : undefined),
+      concatcPhone: (data.contactPhoneNumber ? data.contactPhoneNumber : undefined),
+      contactKinship: (data.contactKinship ? data.contactKinship : undefined)
     };
     this.createBCard(person, personInformation);
   }
 
-  private createPerson(person: Person, personInformation: PersonInformation):void{
-    this._api.createReferencedPerson(person).subscribe(response=>{
-      switch (response.code){
-        case 200:
-          personInformation.id=response.item.id;
-          this.createInformationCollaborator(personInformation, person.email);
-          break;
-        default:
-          this._dialogService.alertDialog('No se pudo acceder', 'Se produjo un error de comunicación con el servidor', 'ACEPTAR');
-          break;
+  private createPerson(person: Person, personInformation: PersonInformation): void {
+    this._api.createReferencedPerson(person).subscribe(response => {
+      if (response.code === HttpResponseCodes.OK) {
+        personInformation.id = response.item.id;
+        this.createInformationCollaborator(personInformation, person.email);
+      } else {
+        this._dialogService.alertDialog('No se pudo acceder', 'Se produjo un error de comunicación con el servidor', 'ACEPTAR');
       }
     });
   }
 
-  private createInformationCollaborator(personInformation: PersonInformation, email:string): void{
-    this._api.savePersonInformation(personInformation).subscribe(response=>{
-      switch (response.code){
-        case 200:
-          this._router.navigate(['/home']).then(() => {
-            this._dialogService.alertDialog(
-              'Información',
-              'Hemos enviado un email de validación de cuenta a: ' + email,
-              'ACEPTAR');
-            this._sharedService.setNotification({value: true, type: SharedTypeNotification.Directory});
-          });
-          break;
-        default:
-          this._dialogService.alertDialog('No se pudo acceder', 'Se produjo un error de comunicación con el servidor', 'ACEPTAR');
-          break;
+  private createInformationCollaborator(personInformation: PersonInformation, email: string): void {
+    this._api.savePersonInformation(personInformation).subscribe(response => {
+      if (response.code === HttpResponseCodes.OK) {
+        this._router.navigate(['/home']).then(() => {
+          this._dialogService.alertDialog(
+            'Información',
+            'Hemos enviado un email de validación de cuenta a: ' + email,
+            'ACEPTAR');
+          this._sharedService.setNotification({value: true, type: SharedTypeNotification.Directory});
+        });
+      } else {
+        this._dialogService.alertDialog('No se pudo acceder', 'Se produjo un error de comunicación con el servidor', 'ACEPTAR');
       }
     });
   }
 
-  public validateEmailExist():void{
-    let email: any = {
+  public validateEmailExist(): void {
+    const email: any = {
       email: this.newPerson.controls['email'].value
     };
-    this._api.personExists(email).subscribe(response=>{
-      switch (response.code){
-        case 200:
-          this._dialogService.alertDialog('Información',
-            'El Email que está tratando de usar ya ha sido asociado a un usuario',
-            'ACEPTAR');
-          this.newPerson.controls['email'].setErrors({emailUsed: true});
-          break;
-        default:
-          break;
+    this._api.personExists(email).subscribe(response => {
+      if (response.code === HttpResponseCodes.OK) {
+        this._dialogService.alertDialog('Información',
+          'El Email que está tratando de usar ya ha sido asociado a un usuario',
+          'ACEPTAR');
+        this.newPerson.controls['email'].setErrors({emailUsed: true});
+      } else {
       }
-    })
+    });
   }
-  public openStudy():void{
+
+  public openStudy(): void {
     this._pdfVisor.open({urlOrFile: this.file});
   }
 
-  private createBCard(person: Person, personInformation: PersonInformation):void{
-    this._snackBarService.openSnackBar('Espere un momento...','',0);
+  private createBCard(person: Person, personInformation: PersonInformation): void {
+    this._snackBarService.openSnackBar('Espere un momento...', '', 0);
     const data = {
       name: person.name || '',
-      lastName: person.lastName  || '',
+      lastName: person.lastName || '',
       company: LocalStorageService.getItem(Constants.StationInDashboard).name || '',
       phone: person.phoneNumber || '',
       workPosition: person.jobTitle || '',
       email: person.email || '',
-      countryCode : person.countryCode || '',
+      countryCode: person.countryCode || '',
       industryCode: '1',
       website: person.website || '',
       profileImage: person.profileImage ? person.profileImage.blobName : null,
-      profileImageThumbnail: person.profileImage ? person.profileImage.thumbnail + '=s1200': null
+      profileImageThumbnail: person.profileImage ? person.profileImage.thumbnail + '=s1200' : null
     };
-    this._api.businessCardService(data).subscribe(response =>{
-      switch (response.code){
-        case 200:
-          this._snackBarService.closeSnackBar();
-          person.bCard = response.item;
-          this.createPerson(person, personInformation);
-          break;
-        default:
-          this._snackBarService.closeSnackBar();
-          this._snackBarService.openSnackBar('Ha ocurrido un error, por favor, intente de nuevo', 'OK', 3000);
-          break;
+    this._api.businessCardService(data).subscribe(response => {
+      if (response.code === HttpResponseCodes.OK) {
+        this._snackBarService.closeSnackBar();
+        person.bCard = response.item;
+        this.createPerson(person, personInformation);
+      } else {
+        this._snackBarService.closeSnackBar();
+        this._snackBarService.openSnackBar('Ha ocurrido un error, por favor, intente de nuevo', 'OK', 3000);
       }
     });
   }
