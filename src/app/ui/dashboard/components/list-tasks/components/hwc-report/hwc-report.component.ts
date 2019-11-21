a90f200506ffe5861c70b7b78b21eb5eca05c89d
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:18:31 -0600

    style: hwc-report component formatted based-TSLint rules and typed

--- src/app/ui/dashboard/components/list-tasks/components/hwc-report/hwc-report.component.ts
+++ src/app/ui/dashboard/components/list-tasks/components/hwc-report/hwc-report.component.ts
@@ -21,6 +21,7 @@ import {HashService} from '@app/utils/utilities/hash.service';
 import {LoaderService} from '@app/core/components/loader/loader.service';
 import {HWCReport} from '@app/utils/interfaces/reports/hwc-report';
 import {Task} from '@app/utils/interfaces/task';
+import {HttpResponseCodes} from '@app/utils/enums/http-response-codes';
 
 @Component({
   selector: 'app-hwc-report',
@@ -32,18 +33,21 @@ export class HwcReportComponent implements OnInit, OnDestroy {
   private _taskId: string;
   public task: Task;
   private _stationId: string;
-  @Input() set taskHwcInfo(taskObj: any){
-    if (taskObj){
+
+  @Input() set taskHwcInfo(taskObj: any) {
+    if (taskObj) {
       this._taskId = taskObj.id;
       this.task = taskObj;
       this.getHWCReport();
     }
   }
-  @Input() set station(station: any){
-    if(station){
+
+  @Input() set station(station: any) {
+    if (station) {
       this._stationId = station.id;
     }
   }
+
   public load: boolean;
   public hwcForm: FormGroup;
   public hwcReport: HWCReport;
@@ -62,8 +66,9 @@ export class HwcReportComponent implements OnInit, OnDestroy {
   private _copyTask: HWCReport;
   private _subscriptionLoader: Subscription;
   private _subscriptionShared: Subscription;
-  private _loads: boolean[];
+  private readonly _loads: boolean[];
   private _files: any;
+
   constructor(
     private _api: ApiService,
     private _apiLoader: LoaderService,
@@ -83,39 +88,39 @@ export class HwcReportComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this._subscriptionLoader = this._apiLoader.getProgress().subscribe(load=>{this.load=load});
+    this._subscriptionLoader = this._apiLoader.getProgress().subscribe(load => {
+      this.load = load;
+    });
     this.initHwcForm();
     this.getNotifications();
   }
 
-  ngOnDestroy():void {
+  ngOnDestroy(): void {
     this._subscriptionShared.unsubscribe();
     this._subscriptionLoader.unsubscribe();
   }
 
-  private getNotifications():void {
-    this._subscriptionShared = this._sharedService.getNotifications().subscribe(response=>{
-      switch (response.type){
-        case SharedTypeNotification.EditTask:
-          if(response.value === 6){
-            this.startEditFormat();
-          }
-          break;
+  private getNotifications(): void {
+    this._subscriptionShared = this._sharedService.getNotifications().subscribe(response => {
+      if (response.type === SharedTypeNotification.EditTask) {
+        if (response.value === 6) {
+          this.startEditFormat();
+        }
       }
     });
   }
 
-  private initHwcForm(): void{
+  private initHwcForm(): void {
     this.hwcForm = this._formBuilder.group({
-      waste:['',[Validators.required]],
-      quantity: ['',[Validators.required]],
-      unity: ['',[Validators.required]],
-      carrierCompany: ['',[Validators.required]],
-      finalDestination: ['',[Validators.required]]
+      waste: ['', [Validators.required]],
+      quantity: ['', [Validators.required]],
+      unity: ['', [Validators.required]],
+      carrierCompany: ['', [Validators.required]],
+      finalDestination: ['', [Validators.required]]
     });
   }
 
-  private patchForm(report: HWCReport): void{
+  private patchForm(report: HWCReport): void {
     this.hwcReport = {
       carrierCompany: report.carrierCompany || null,
       date: report.date || null,
@@ -144,50 +149,47 @@ export class HwcReportComponent implements OnInit, OnDestroy {
     this.hwcForm.disable();
   }
 
-  private getHWCReport(): void{
-    this._api.getTaskInformation(this._taskId, 6).subscribe(response=>{
-      switch (response.code){
-        case 200:
-          if(response.items){
-            this.taskItems = UtilitiesService.sortJSON(response.items, 'folio', 'desc');
-            this._indexTask = 0;
-            this.patchForm(this.taskItems[0])
-          }else{
-            this.resetElements();
-          }
-          break;
-        default:
+  private getHWCReport(): void {
+    this._api.getTaskInformation(this._taskId, 6).subscribe(response => {
+      if (response.code === HttpResponseCodes.OK) {
+        if (response.items) {
+          this.taskItems = UtilitiesService.sortJSON(response.items, 'folio', 'desc');
+          this._indexTask = 0;
+          this.patchForm(this.taskItems[0]);
+        } else {
           this.resetElements();
-          break;
+        }
+      } else {
+        this.resetElements();
       }
-    })
+    });
   }
 
-  private resetElements(): void{
+  private resetElements(): void {
     this.hwcReport = undefined;
     this.hwcForm.reset();
     this.hwcForm.disable();
     const user = LocalStorageService.getItem(Constants.UserInSession);
-    if(this.task.status !== 4 && user.role ===7){
+    if (this.task.status !== 4 && user.role === 7) {
       this.startEditFormat(true);
     }
   }
 
-  public changeTask(ev: any){
+  public changeTask(ev: any) {
     this._indexTask = ev.pageIndex;
     this.patchForm(this.taskItems[this._indexTask]);
   }
 
-  private startEditFormat(isNewLoad?: boolean):void {
+  private startEditFormat(isNewLoad?: boolean): void {
     let today: any = new Date();
     const user = LocalStorageService.getItem(Constants.UserInSession);
     today = UtilitiesService.createPersonalTimeStamp(today);
     this.date = UtilitiesService.convertDate(today.timeStamp);
     this.editable = true;
     this.name = user.completeName;
-    if(!isNewLoad){
+    if (!isNewLoad) {
       this._copyTask = this.hwcReport;
-      if(this.hwcReport.fileCS){
+      if (this.hwcReport.fileCS) {
         this._fileElement = this.hwcReport.fileCS;
         this.file = true;
       }
@@ -200,59 +202,57 @@ export class HwcReportComponent implements OnInit, OnDestroy {
 
   public loadSignature(): void {
     this._signatureService.open().afterClosed().subscribe(response => {
-      switch (response.code) {
-        case 1:
-          this.signatureThumbnail = response.base64;
-          this._loads[1] = true;
-          this._signature = new FormData();
-          this._signature.append('fileName', 'signature-' + new Date().getTime() + '.png');
-          this._signature.append('isImage', 'true');
-          this._signature.append('file', response.blob);
-          break;
+      if (response.code === 1) {
+        this.signatureThumbnail = response.base64;
+        this._loads[1] = true;
+        this._signature = new FormData();
+        this._signature.append('fileName', 'signature-' + new Date().getTime() + '.png');
+        this._signature.append('isImage', 'true');
+        this._signature.append('file', response.blob);
       }
     });
   }
 
-  public loadFile(ev: UploadFileResponse):void {
+  public loadFile(ev: UploadFileResponse): void {
     this.file = true;
     this._loads[0] = true;
     this._files = ev.file;
     this._file = new FormData();
-    this._file.append('path','Taks-'+this._taskId);
-    this._file.append('fileName', 'manifest-'+this._taskId+new Date().getTime()+'.pdf');
+    this._file.append('path', 'Taks-' + this._taskId);
+    this._file.append('fileName', 'manifest-' + this._taskId + new Date().getTime() + '.pdf');
     this._file.append('file', ev.file);
     this.error = false;
   }
 
 
-  public validateForm(value: any): void{
-    if(!this.file){
+  public validateForm(value: any): void {
+    if (!this.file) {
       this.error = true;
     }
-    if(this.error || this.hwcForm.invalid){
-      this._snackBarService.openSnackBar('Por favor, complete los campos','OK',3000);
+    if (this.error || this.hwcForm.invalid) {
+      this._snackBarService.openSnackBar('Por favor, complete los campos', 'OK', 3000);
       return;
     }
-    if(!this._signature){
-      this._snackBarService.openSnackBar('Por favor, registre su firma','OK',3000);
+    if (!this._signature) {
+      this._snackBarService.openSnackBar('Por favor, registre su firma', 'OK', 3000);
       return;
     }
-    if(this._loads[0]){
+    if (this._loads[0]) {
       this.uploadFile(1);
       return;
     }
-    if(this._loads[1]){
+    if (this._loads[1]) {
       this.uploadFile(2);
       return;
     }
     this.saveReport(value);
   }
 
-  private uploadFile(type: number):void{
-    switch (type){
+  private uploadFile(type: number): void {
+    switch (type) {
       case 1:
-        this._uploadFile.upload(this._file).subscribe(response=>{
-          if (response){
+        this._uploadFile.upload(this._file).subscribe(response => {
+          if (response) {
             this._fileElement = {
               blobName: response.item.blobName,
               thumbnail: response.item.thumbnail
@@ -263,8 +263,8 @@ export class HwcReportComponent implements OnInit, OnDestroy {
         });
         break;
       case 2:
-        this._uploadFile.upload(this._signature).subscribe(response=>{
-          if (response){
+        this._uploadFile.upload(this._signature).subscribe(response => {
+          if (response) {
             this._signatureElement = {
               blobName: response.item.blobName,
               thumbnail: response.item.thumbnail
@@ -279,7 +279,7 @@ export class HwcReportComponent implements OnInit, OnDestroy {
     }
   }
 
-  private saveReport(value: any): void{
+  private saveReport(value: any): void {
     let date: any = new Date();
     date = UtilitiesService.createPersonalTimeStamp(date);
     this.hwcReport = {
@@ -294,45 +294,39 @@ export class HwcReportComponent implements OnInit, OnDestroy {
       unity: value.unity,
       waste: value.waste
     };
-    if(this._copyTask){
+    if (this._copyTask) {
       this.hwcReport.id = this._copyTask.id;
-      this._api.createTask(this.hwcReport, 6).subscribe(response=>{
-        switch (response.code){
-          case 200:
-            this._sharedService.setNotification({type: SharedTypeNotification.FinishEditTask, value: response.item.station});
-            break;
-          default:
-            console.error(response);
-            break;
+      this._api.createTask(this.hwcReport, 6).subscribe(response => {
+        if (response.code === HttpResponseCodes.OK) {
+          this._sharedService.setNotification({type: SharedTypeNotification.FinishEditTask, value: response.item.station});
+        } else {
+          console.error(response);
         }
       });
-    }else{
-      this._api.createHWCReportAndTask(this.hwcReport,this._stationId).subscribe(response=>{
-        switch (response.code){
-          case 200:
-            this._sharedService.setNotification({type: SharedTypeNotification.FinishEditTask, value: response.item.station});
-            break;
-          default:
-            console.error(response);
-            break;
+    } else {
+      this._api.createHWCReportAndTask(this.hwcReport, this._stationId).subscribe(response => {
+        if (response.code === HttpResponseCodes.OK) {
+          this._sharedService.setNotification({type: SharedTypeNotification.FinishEditTask, value: response.item.station});
+        } else {
+          console.error(response);
         }
-      })
+      });
     }
   }
 
-  public seeReport(): void{
+  public seeReport(): void {
     const user = LocalStorageService.getItem(Constants.UserInSession);
-    switch (user.role){
+    switch (user.role) {
       case 1:
       case 2:
       case 4:
       case 7:
-        this._pdf.open({urlOrFile: this._loads[0] ? this._files : HashService.set('123456$#@$^@1ERF',this.hwcReport.fileCS.thumbnail)});
+        this._pdf.open({urlOrFile: this._loads[0] ? this._files : HashService.set('123456$#@$^@1ERF', this.hwcReport.fileCS.thumbnail)});
         break;
       case 3:
       case 5:
       case 6:
-        this._pdf.open({urlOrFile: HashService.set('123456$#@$^@1ERF',this.hwcReport.fileCS.thumbnail), hideDownload: true});
+        this._pdf.open({urlOrFile: HashService.set('123456$#@$^@1ERF', this.hwcReport.fileCS.thumbnail), hideDownload: true});
         break;
     }
   }
