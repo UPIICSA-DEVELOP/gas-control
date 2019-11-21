7e12a088aafb5b20a50948bee8ca5f81340ca88d
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 12:08:11 -0600

    style: main.ts file formatted based-TSLint rules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppBrowserModule } from './app/app.browser.module';
-import { environment } from './environments/environment';
-import 'hammerjs'
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppBrowserModule} from '@app/app.browser.module';
+import {environment} from '@env/environment';
+import 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
