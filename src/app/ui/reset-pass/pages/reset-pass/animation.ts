42270fa8a5c64b278dee44ac30949ef104cc9798
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:49:28 -0600

    refactor: reset-pass component animation file added

---
+++ src/app/ui/reset-pass/pages/reset-pass/animation.ts
@@ -0,0 +1,24 @@
+import {animate, keyframes, query, stagger, style, transition, trigger} from '@angular/animations';
+
+export const ANIMATION = [
+  trigger('fadeInAnimation', [
+    transition(':enter', [
+      query('#reset-password', style({opacity: 0, background: 'transparent'}), {optional: true}),
+      query('#reset-password', stagger('10ms', [
+        animate('.2s ease-out', keyframes([
+          style({opacity: 0, background: 'transparent', offset: 0}),
+          style({opacity: .5, background: 'rgba(255, 255, 255, .5)', offset: 0.5}),
+          style({opacity: 1, background: 'rgba(255, 255, 255, 1)', offset: 1.0}),
+        ]))]), {optional: true})
+    ]),
+    transition(':leave', [
+      query('#reset-password', style({opacity: 1, background: 'rgba(255, 255, 255, 1)'}), {optional: true}),
+      query('#reset-password', stagger('10ms', [
+        animate('.2s ease-in', keyframes([
+          style({opacity: 1, background: 'rgba(255, 255, 255, 1)', offset: 0}),
+          style({opacity: .5, background: 'rgba(255, 255, 255, .5)', offset: 0.5}),
+          style({opacity: 0, background: 'transparent', offset: 1.0}),
+        ]))]), {optional: true})
+    ])
+  ])
+];
