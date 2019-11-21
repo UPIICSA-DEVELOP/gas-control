83082daef5f16cfec512d194814a27c6e1b4fc71
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:52:26 -0600

    style: EntityCollectionResponse class formatted based-TSLint rules

--- src/app/utils/class/entity-collection-response.ts
+++ src/app/utils/class/entity-collection-response.ts
@@ -1,7 +1,7 @@
 import {DefaultResponse} from '@app/utils/interfaces/default-response';
 import {HttpResponseCodes} from '@app/utils/enums/http-response-codes';
 
-export class EntityCollectionResponse<T> implements DefaultResponse{
+export class EntityCollectionResponse<T> implements DefaultResponse {
   code: HttpResponseCodes;
   description: string;
   items: T[];
