ca0855fb9eeca959b75a0d9ad0a48b1234d55b8f
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:40:22 -0600

    style: procedure component formatted based-TSLint rules and typed

--- src/app/ui/dashboard/pages/procedures/procedures.component.ts
+++ src/app/ui/dashboard/pages/procedures/procedures.component.ts
@@ -4,37 +4,29 @@
  * Proprietary and confidential
  */
 
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, HostBinding, OnInit, ViewEncapsulation} from '@angular/core';
 import {ApiService} from 'app/core/services/api/api.service';
 import {Router} from '@angular/router';
-import {animate, style, transition, trigger} from '@angular/animations';
 import {PdfVisorService} from 'app/shared/components/pdf-visor/pdf-visor.service';
 import {Constants} from 'app/utils/constants/constants.utils';
 import {LocalStorageService} from 'app/core/services/local-storage/local-storage.service';
 import {HashService} from 'app/utils/utilities/hash.service';
 import {Procedure} from '@app/utils/interfaces/procedure';
+import {HttpResponseCodes} from '@app/utils/enums/http-response-codes';
+import {ANIMATION} from '@app/ui/dashboard/pages/procedures/animation';
 
 @Component({
   selector: 'app-procedures',
   templateUrl: './procedures.component.html',
   styleUrls: ['./procedures.component.scss'],
-  animations: [
-    trigger('fadeInAnimation', [
-      transition(':enter', [
-        style({ right: '-100%' }),
-        animate('.40s ease-out', style({ right: '0'  }))
-      ]),
-      transition(':leave', [
-        style({ right: '0'}),
-        animate('.40s ease-in', style({ right: '-100%' }))
-      ])
-    ])
-  ],
-  host: {'[@fadeInAnimation]': ''},
+  animations: [ANIMATION],
   encapsulation: ViewEncapsulation.None
 })
 export class ProceduresComponent implements OnInit {
+  @HostBinding('@fadeInAnimation')
+
   public procedures: Procedure[];
+
   constructor(
     private _api: ApiService,
     private _route: Router,
@@ -47,35 +39,33 @@ export class ProceduresComponent implements OnInit {
     this.getProcedures();
   }
 
-  public onCloseProcedures():void{
-    this._route.navigate(['/home']);
+  public onCloseProcedures(): void {
+    this._route.navigate(['/home']).then();
   }
 
-  public openFile(url:any):void{
+  public openFile(url: any): void {
     const user = LocalStorageService.getItem(Constants.UserInSession);
-    switch (user.role){
+    switch (user.role) {
       case 1:
       case 2:
       case 7:
-        this._pdf.open({urlOrFile: HashService.set("123456$#@$^@1ERF", url)});
+        this._pdf.open({urlOrFile: HashService.set('123456$#@$^@1ERF', url)});
         break;
       case 3:
       case 4:
       case 5:
       case 6:
-        this._pdf.open({urlOrFile: HashService.set("123456$#@$^@1ERF", url),hideDownload: true});
+        this._pdf.open({urlOrFile: HashService.set('123456$#@$^@1ERF', url), hideDownload: true});
         break;
     }
   }
 
-  private getProcedures():void{
-    this._api.getUtils().subscribe(response=>{
-      switch (response.code){
-        case 200:
-          this.procedures = response.item.procedures;
-          break;
+  private getProcedures(): void {
+    this._api.getUtils().subscribe(response => {
+      if (response.code === HttpResponseCodes.OK) {
+        this.procedures = response.item.procedures;
       }
-    })
+    });
   }
 
 }
