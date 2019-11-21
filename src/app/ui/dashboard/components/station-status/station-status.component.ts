555d16d7163df20efa090faf50d7551af9b1d22c
Israel Olvera <skrigueztep@gmail.com>
2019-11-21 11:25:45 -0600

    style: station-status component formatted based-TSLint rules and typed

--- src/app/ui/dashboard/components/station-status/station-status.component.ts
+++ src/app/ui/dashboard/components/station-status/station-status.component.ts
@@ -9,7 +9,7 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import {isPlatformBrowser} from '@angular/common';
-import { Chart } from 'chart.js';
+import {Chart} from 'chart.js';
 import {SharedService, SharedTypeNotification} from '@app/core/services/shared/shared.service';
 import {Subscription} from 'rxjs';
 import {Station} from '@app/utils/interfaces/station';
@@ -21,62 +21,62 @@ import {Station} from '@app/utils/interfaces/station';
   styleUrls: ['./station-status.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class StationStatusComponent implements OnInit, AfterViewInit, OnDestroy{
+export class StationStatusComponent implements OnInit, AfterViewInit, OnDestroy {
   private _station: Station;
-  @Input() set station(stationObj: Station){
-    if(stationObj){
+  @Input() set station(stationObj: Station) {
+    if (stationObj) {
       this._station = stationObj;
-      if(this._canvas){
+      if (this._canvas) {
         this.createConfigGraphic(this._station);
       }
     }
-  } ;
-  @ViewChild('canvas', { static: false }) private _canvas: ElementRef;
-  @ViewChild('legend', { static: true }) private _legend: ElementRef;
+  }
+
+  @ViewChild('canvas', {static: false}) private _canvas: ElementRef;
+  @ViewChild('legend', {static: true}) private _legend: ElementRef;
   public showGraphic: boolean;
   public data: any;
   public chart: any;
   private _chartConfig: any;
   private _subscriptionShared: Subscription;
+
   constructor(
     @Inject(PLATFORM_ID) private _platformId,
     private _sharedService: SharedService
-  ){
+  ) {
     this.showGraphic = isPlatformBrowser(this._platformId);
   }
 
   ngOnInit(): void {
-    this._subscriptionShared = this._sharedService.getNotifications().subscribe(response=>{
-      switch (response.type){
-        case SharedTypeNotification.FinishEditTask:
-          this._station.doneTasks = response.value.doneTasks;
-          this._station.progress = response.value.progress;
-          this.createConfigGraphic(this._station);
-          break;
+    this._subscriptionShared = this._sharedService.getNotifications().subscribe(response => {
+      if (response.type === SharedTypeNotification.FinishEditTask) {
+        this._station.doneTasks = response.value.doneTasks;
+        this._station.progress = response.value.progress;
+        this.createConfigGraphic(this._station);
       }
     });
   }
 
-  ngAfterViewInit():void{
-    if(this._station){
+  ngAfterViewInit(): void {
+    if (this._station) {
       this.createConfigGraphic(this._station);
     }
   }
 
-  ngOnDestroy(): void{
+  ngOnDestroy(): void {
     this._subscriptionShared.unsubscribe();
   }
 
-  private createConfigGraphic(station: any){
+  private createConfigGraphic(station: any) {
     this._chartConfig = {
       type: 'doughnut',
       data: {
-        labels: ["Completas", "Incompletas"],
+        labels: ['Completas', 'Incompletas'],
         datasets: [
           {
-            data: [station.doneTasks, station.totalTasks-station.doneTasks],
+            data: [station.doneTasks, station.totalTasks - station.doneTasks],
             borderColor: '#FFFFFF',
-            backgroundColor: ['#00C500','#707070'],
+            backgroundColor: ['#00C500', '#707070'],
             fill: false
           }
         ]
@@ -86,29 +86,29 @@ export class StationStatusComponent implements OnInit, AfterViewInit, OnDestroy{
         responsive: true,
         aspectRatio: 1,
         legend: false,
-        legendCallback: function(chart) {
+        legendCallback: function (chart) {
           const text: string[] = [];
           text.push('<ul style="font-size: 200%; line-height: 1.25; margin: 0;">');
-          for(let y=0; y < chart.data.datasets[0].data.length; y++){
-            text.push("<li style='color:"+chart.data.datasets[0].backgroundColor[y]+";'><span style='color: #707070; font-size: 50%'>" + chart.data.labels[y] +"</span></li>");
+          for (let y = 0; y < chart.data.datasets[0].data.length; y++) {
+            text.push('<li style=\'color:' + chart.data.datasets[0].backgroundColor[y] + ';\'><span style=\'color: #707070; font-size: 50%\'>' + chart.data.labels[y] + '</span></li>');
           }
           text.push('</ul>');
-          return text.join("");
+          return text.join('');
         }
       },
-      plugins:{
+      plugins: {
         beforeDraw: (chart) => {
-          let width = chart.chart.width,
+          const width = chart.chart.width,
             height = chart.chart.height,
             ctx = chart.chart.ctx;
 
           ctx.restore();
-          let fontSize = (height / 80).toFixed(2);
-          ctx.font = fontSize + "em sans-serif";
-          ctx.textBaseline = "middle";
+          const fontSize = (height / 80).toFixed(2);
+          ctx.font = fontSize + 'em sans-serif';
+          ctx.textBaseline = 'middle';
           ctx.fillStyle = '#0d47a1';
 
-          let text = station.progress + '%',
+          const text = station.progress + '%',
             textX = Math.round((width - ctx.measureText(text).width) / 2),
             textY = height / 2;
 
@@ -121,10 +121,10 @@ export class StationStatusComponent implements OnInit, AfterViewInit, OnDestroy{
     this.createGraphic();
   }
 
-  private createGraphic(): void{
-    if(isPlatformBrowser(this._platformId)){
-      if(this.chart){
-        this.chart.render();
+  private createGraphic(): void {
+    if (isPlatformBrowser(this._platformId)) {
+      if (this.chart) {
+        this.chart.render().then();
         this.chart.destroy();
         this.chart = null;
       }
@@ -132,7 +132,6 @@ export class StationStatusComponent implements OnInit, AfterViewInit, OnDestroy{
       this._legend.nativeElement.innerHTML = this.chart.generateLegend();
     }
   }
-
 
 
 }
