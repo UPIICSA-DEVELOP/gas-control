025d34173389a01c4143cede2b76c9d9e349e295
Alejandro López A <program.alopez@gmail.com>
2020-03-26 20:52:15 -0600

    fix(bin): Remove api task of all scripts

--- bin/build-prod.sh
+++ bin/build-prod.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 export NODE_ENV=prod
 export PORT=9090
-export FRONT_URL='https://app.inspectordenormas.com/',
-export BACKEND_URL='https://inspector-backend.appspot.com/_ah/api/communication/v1/'
 gulp clean
 ng build --prod
 ng run com.maplander.app.inspector.front:server:production
-npm run api
 npm run config
 npm run server
 exit 0
--- bin/ssr.sh
+++ bin/ssr.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 export NODE_ENV=dev
 export PORT=8090
-export FRONT_URL='https://inspector-develop.maplander.com/',
-export BACKEND_URL='https://schedule-maplander.appspot.com/_ah/api/communication/v1/'
 gulp clean
 ng build -c develop
 ng run com.maplander.app.inspector.front:server:develop
-npm run api
 npm run config
 npm run server
 exit 0
