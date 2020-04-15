f62047f2ca57b499d12d0311b43a28eb8f3d8efb
Alejandro López A <program.alopez@gmail.com>
2020-04-15 11:57:35 -0500

    fix(auth-router.service): Remove local validateUserInSession method

--- src/app/core/services/auth/auth-router.service.ts
+++ src/app/core/services/auth/auth-router.service.ts
@@ -2,8 +2,9 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Constants} from 'app/utils/constants/constants.utils';
-import {CookieService, LocalStorageService} from '@maplander/core';
+import {LocalStorageService} from '@maplander/core';
 import {Person} from '@app/utils/interfaces/person';
+import {AuthService} from '@app/core/services/auth/auth.service';
 
 @Injectable()
 export class AuthRouterService implements CanActivate {
@@ -13,14 +14,10 @@ export class AuthRouterService implements CanActivate {
   ) {
   }
 
-  private static validateUserInSession(): boolean {
-    return CookieService.getCookie(Constants.IdSession) !== null;
-  }
-
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
     : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (state.url === '/login') {
-      if (AuthRouterService.validateUserInSession()) {
+      if (AuthService.validateUserInSession()) {
         if (LocalStorageService.getItem<Person>(Constants.UserInSession).role === 7) {
           return this._router.createUrlTree(['/admin']);
         } else {
@@ -31,11 +28,11 @@ export class AuthRouterService implements CanActivate {
       }
     } else if (
       state.url.includes('/admin') &&
-      AuthRouterService.validateUserInSession() &&
+      AuthService.validateUserInSession() &&
       LocalStorageService.getItem<Person>(Constants.UserInSession).role !== 7) {
       return this._router.createUrlTree(['/home']);
     } else {
-      if (AuthRouterService.validateUserInSession()) {
+      if (AuthService.validateUserInSession()) {
         return true;
       } else {
         return this._router.createUrlTree(['/login']);
