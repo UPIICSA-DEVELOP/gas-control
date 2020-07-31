f9edaf1c2f034d6ff559e32163454eec40064d18
Fernando Flores <feruriel00@hotmail.com>
2020-07-31 16:09:50 -0500

    fix(fe-report.component): change type task

--- src/app/ui/dashboard/components/list-tasks/components/fe-report/fe-report.component.ts
+++ src/app/ui/dashboard/components/list-tasks/components/fe-report/fe-report.component.ts
@@ -29,7 +29,7 @@ import {Person} from '@app/utils/interfaces/person';
 })
 export class FeReportComponent implements OnInit, OnDestroy {
   private _taskId: string;
-  public task: Task;
+  public task: any;
 
   @Input() set taskFeInfo(taskObj: any) {
     if (taskObj) {
