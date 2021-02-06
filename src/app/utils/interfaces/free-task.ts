c9abed35e3ccf5247b7c6e413b6dd834a8d1115f
ANGEL <jaramillogonzalezangel@gmail.com>
2021-02-05 20:02:05 -0600

    Add(free-task)Add free-task model

---
+++ src/app/utils/interfaces/free-task.ts
@@ -0,0 +1,15 @@
+import {FileCS} from '@app/utils/interfaces/file-cs';
+
+export interface FreeTask {
+  area: string;
+  date: number;
+  description: string;
+  fileCS: FileCS;
+  folio?: number;
+  id?: string;
+  name: string;
+  procedures?: number[];
+  signature: FileCS;
+  taskId: string;
+  time: number;
+}
